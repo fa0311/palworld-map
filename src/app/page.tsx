4e5387fe035c72166b6004ecb40850fef419a169
ふぁ <yuki@yuki0311.com>
2024-04-06 02:20:52 +0900

    update
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
   useEffect(() => {
     getPlayers();
-    const interval = setInterval(getPlayers, 1000);
+    const interval = setInterval(getPlayers, 1500);
     return () => clearInterval(interval);
   }, []);
 
--- src/app/status/route.ts
+++ src/app/status/route.ts
@@ -3,7 +3,7 @@ import { Configuration, InfoApi } from "palworld-openapi";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const config = new Configuration({
-    fetchApi: (x, y) => fetch(x, { ...y, next: { revalidate: 0.8 } }),
+    fetchApi: (x, y) => fetch(x, { ...y, next: { revalidate: 1 } }),
     basePath: process.env.BASE_PATH,
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
