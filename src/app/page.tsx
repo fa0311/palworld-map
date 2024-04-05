694cde1778cd9345db5b6b6a370035e225cc52da
ふぁ <yuki@yuki0311.com>
2024-04-06 01:12:33 +0900

    update
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,6 @@ import dynamic from "next/dynamic";
 import { type Player } from "palworld-openapi";
 import { useEffect, useState } from "react";
 
-// const MapContainer = dynamic(
-//   () => import("react-leaflet").then((m) => m.MapContainer),
-//   { ssr: false }
-// );
-// const TileLayer = dynamic(
-//   () => import("react-leaflet").then((m) => m.TileLayer),
-//   { ssr: false }
-// );
-// const Marker = dynamic(() => import("react-leaflet").then((m) => m.Marker), {
-//   ssr: false,
-// });
-// const Popup = dynamic(() => import("react-leaflet").then((m) => m.Popup), {
-//   ssr: false,
-// });
-// const Marker = dynamic(() => import("./leaflet").then((m) => m.Marker), {
-//   ssr: false,
-// });
-
 const Leaflet = dynamic(() => import("./leaflet").then((m) => m.Leaflet), {
   ssr: false,
 });
